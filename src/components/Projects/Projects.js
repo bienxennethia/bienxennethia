aef366259e29a90dc155bfd4d821b5132259a738
Xennethia Bien <bienxennethia@gmail.com>
2023-05-05 14:11:45 +0800

    updated project links

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,9 +10,9 @@ const Projects = () => {
         <h1 className="projects__title title">Projects</h1>
         <div className="projects__grid">
           <Project title="The Venetian Resort Las Vegas" position="Full-Stack" copy="The Venetian Las Vegas is a luxury hotel and casino resort that offers shops, shows, and additional attractions all set within architectural replicas of some of Venice's greatest monuments located on the Las Vegas Strip in Paradise, Nevada, United States." image={require('./Images/venetian.png')} link="https://www.venetianlasvegas.com/" />
-          <Project title="Sands" position="Back-End" copy="Las Vegas Sands Corporation is an American casino and resort company with corporate headquarters in Paradise, Nevada, United States." image={require('./Images/sands.png')} link="https://www.venetianlasvegas.com/" />
-          <Project title="Clear Labs" position="Full-Stack" copy="Clear Labs, Inc. is a private genomics testing company headquartered in San Carlos, California, United States." image={require('./Images/clearlabs.png')} link="https://www.venetianlasvegas.com/" />
-          <Project title="Skidmore, Owings & Merrill (SOM)" position="Front-End" copy="Skidmore, Owings & Merrill is an American architectural, urban planning and engineering firm." image={require('./Images/som.png')} link="https://www.venetianlasvegas.com/" />
+          <Project title="Sands" position="Back-End" copy="Las Vegas Sands Corporation is an American casino and resort company with corporate headquarters in Paradise, Nevada, United States." image={require('./Images/sands.png')} link="https://www.sands.com/" />
+          <Project title="Clear Labs" position="Full-Stack" copy="Clear Labs, Inc. is a private genomics testing company headquartered in San Carlos, California, United States." image={require('./Images/clearlabs.png')} link="https://www.clearlabs.com/" />
+          <Project title="Skidmore, Owings & Merrill (SOM)" position="Front-End" copy="Skidmore, Owings & Merrill is an American architectural, urban planning and engineering firm." image={require('./Images/som.png')} link="https://www.som.com/" />
         </div>
       </div>
     </section>
