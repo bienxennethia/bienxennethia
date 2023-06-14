e713616468fece33317ba34b108bbabaf42fa43c
Xennethia Bien <bienxennethia@gmail.com>
2023-06-14 05:17:02 +0000

    initial deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Removing build and packages files..";
+rm -rf build;
+rm -rf node_modules;
+
+# install nvm and node
+source $NVM_DIR/nvm.sh;
+nvm use 16.18.0;
+
+# install npm packages
+echo "Installing npm packages..";
+npm install;
+
+# building the app
+echo "Building the app..";
+npm run build;
+
+# starting the app
+echo "Starting the app..";
+npm run start;
